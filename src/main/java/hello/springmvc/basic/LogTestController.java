e5eafe25b0c0ef928f79c89d289759f29154cea0
Dayeong <dayeongmaeng@gmail.com>
2023-04-13 14:56:39 +0900

    feat: 로깅 간단히 알아보기

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,24 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+    @GetMapping("/log-test")
+    public String logTest() {
+
+        //개발 서버는 debug, 운영 서버는 info로 보통 설정
+        String name = "Spring";
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
